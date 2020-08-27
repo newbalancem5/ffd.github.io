6c82060a47d3d394d2331c47e7899fdd8b157cd1
Roman <newbalancem5@gmail.com>
2020-08-27 21:57:29 +0300

    upd

--- lib/HyperLink/Link.dart
+++ lib/HyperLink/Link.dart
@@ -1,68 +1,60 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:flutter/material.dart';
-import 'dart:html' as html;
+// import 'package:flutter/material.dart';
+// import 'dart:html' as html;
 
-class HyperLinks extends StatefulWidget {
+// class HyperLinks extends StatefulWidget {
 
-  final String href;
-  final String nameLink;
-  final List<Widget> child;
+//   final String href;
+//   final String nameLink;
+//   final List<Widget> child;
 
+//   HyperLinks({
+//     this.href,
+//     this.child,
+//     this.nameLink,
+//   });
 
+//   @override
+//   _HyperLinksState createState() => _HyperLinksState();
+// }
 
+// class _HyperLinksState extends State<HyperLinks> {
 
-  HyperLinks({
-    this.href,
-    this.child,
-    this.nameLink,
-  });
+//   final url = html.window.open;
 
-  @override
-  _HyperLinksState createState() => _HyperLinksState();
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: _linkButton()
+//     );
+//   }
 
-class _HyperLinksState extends State<HyperLinks> {
+//   Widget _linkButton(){
 
-  final url = html.window.open;
+//     return GestureDetector(
+//           child: _textLink(),
+//           onTap: () {
 
+//             url('','23');
 
+//           });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: _linkButton()
-    );
-  }
+//   Widget _textLink(){
+//     return Text(
+//         widget.nameLink,
+//         style: TextStyle(
+//                   decoration: TextDecoration.none,
+//                   color: Colors.blue,
+//                   fontSize: 20)
+//       );
+//   }
 
-  Widget _linkButton(){
+//   Widget  _href(){
+//     return Text(
+//         widget.href
+//       );
+//   }
 
-    
-    return GestureDetector(
-          child: _textLink(),
-          onTap: () {
-
-            url('','23');
-
-          });
-  }
-
-
-  Widget _textLink(){
-    return Text(
-        widget.nameLink,
-        style: TextStyle(
-                  decoration: TextDecoration.none,
-                  color: Colors.blue,
-                  fontSize: 20)
-      );
-  }
-
-  Widget  _href(){
-    return Text(
-        widget.href
-      );
-  }
-
-
-}
+// }
--- lib/HyperLink/SocialLink.dart
+++ lib/HyperLink/SocialLink.dart
@@ -1,14 +1,14 @@
-import 'package:ffd/HyperLink/Link.dart';
-import 'package:flutter/material.dart';
+// import 'package:ffd/HyperLink/Link.dart';
+// import 'package:flutter/material.dart';
 
-class SocialLink extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: HyperLinks(
-        nameLink: 'telegram',
-        href: 'https://stackoverflow.com/',
-      ),
-    );
-  }
-}
+// class SocialLink extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: HyperLinks(
+//         nameLink: 'telegram',
+//         href: 'https://stackoverflow.com/',
+//       ),
+//     );
+//   }
+// }
