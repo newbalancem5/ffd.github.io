04e127848560a5f30ed92423707f3bf72845a0d2
newbalancem5 <newbalancem5@gmail.com>
2022-08-23 13:41:51 +0300

    upd

--- lib/Screen/TitleBlock.dart
+++ lib/Screen/TitleBlock.dart
@@ -1,4 +1,3 @@
-import 'package:ffd/const/size.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
--- lib/Screen/TitleInfo.dart
+++ lib/Screen/TitleInfo.dart
@@ -5,16 +5,15 @@ class TitleInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(
-        ("info.title"),
-        style: GoogleFonts.robotoSlab(
-          color: Colors.black,
-          fontStyle: FontStyle.normal,
-          fontSize: 20,
-          decoration: TextDecoration.none,
-        ),
-        textAlign: TextAlign.center,
+        child: Text(
+      ("info.title"),
+      style: GoogleFonts.robotoSlab(
+        color: Colors.black,
+        fontStyle: FontStyle.normal,
+        fontSize: 20,
+        decoration: TextDecoration.none,
       ),
-    );
+      textAlign: TextAlign.center,
+    ));
   }
 }
